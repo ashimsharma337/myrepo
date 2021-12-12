7ecdee5fe9159df7c303a52fb4f07a43073b9fb5
Ashim Sharma <ashimsharma337@gmail.com>
2021-12-12 19:51:39 +0545

    getting json data

--- Express_js/index.js
+++ Express_js/index.js
@@ -3,6 +3,30 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+const members = [
+    {
+        id: 1,
+        name: "John Doe",
+        email: "johndoe@email.com",
+        status: "active"
+    },
+    {
+        id: 2,
+        name: "Bob Williams",
+        email: "boewille@email.com",
+        status: "inactive"
+    },
+    {
+        id: 3,
+        name: "sharon lee",
+        email: "leesharon@email.com",
+        status: "active"
+    },
+];
+
+// Gets all members
+app.get("/api/members", (req, res) => res.json(members));
+
 
 // Set static folder
 app.use(express.static(process.cwd()+"/Public"));
