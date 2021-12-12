9e32dae03aa2dcec1626d94ad3194d68d27692ec
Ashim Sharma <ashimsharma337@gmail.com>
2021-12-12 19:39:39 +0545

    static server created

--- Express_js/index.js
+++ Express_js/index.js
@@ -4,9 +4,8 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 
-app.get("/", (req, res) => {
-    res.sendFile(process.cwd()+"/Public/index.html");
-});
+// Set static folder
+app.use(express.static(process.cwd()+"/Public"));
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT} ......`)
