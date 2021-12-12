adc7e2389a3f429926c5194947462bdf0c6cffd5
Ashim Sharma <ashimsharma337@gmail.com>
2021-12-12 20:08:25 +0545

    middleware example

---
+++ Express_js/Members.js
@@ -0,0 +1,23 @@
+
+const members = [
+    {
+        id: 1,
+        name: "John Doe",
+        email: "johndoe@email.com",
+        status: "active"
+    },
+    {
+        id: 2,
+        name: "Bob Williams",
+        email: "boewille@email.com",
+        status: "inactive"
+    },
+    {
+        id: 3,
+        name: "sharon lee",
+        email: "leesharon@email.com",
+        status: "active"
+    },
+];
+
+export { members };
--- Express_js/index.js
+++ Express_js/index.js
@@ -1,28 +1,17 @@
 import express from "express";
+import { members } from "./Members.js";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-const members = [
-    {
-        id: 1,
-        name: "John Doe",
-        email: "johndoe@email.com",
-        status: "active"
-    },
-    {
-        id: 2,
-        name: "Bob Williams",
-        email: "boewille@email.com",
-        status: "inactive"
-    },
-    {
-        id: 3,
-        name: "sharon lee",
-        email: "leesharon@email.com",
-        status: "active"
-    },
-];
+// creating middleware
+const logger = (req, res, next) => {
+     console.log(`${req.protocol}://${req.get("host")}${req.originalUrl}`);
+     next();
+};
+
+// Init middleware
+app.use(logger);
 
 // Gets all members
 app.get("/api/members", (req, res) => res.json(members));
