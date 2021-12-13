68f12e7ea9dababce82dfca203a8f00e97708db2
Ashim Sharma <ashimsharma337@gmail.com>
2021-12-13 11:13:23 +0545

    new member created with uuid

--- Express_js/index.js
+++ Express_js/index.js
@@ -9,6 +9,11 @@ const PORT = process.env.PORT || 8000;
 // Init middleware
 // app.use(logger);
 
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
 // Set static folder
 app.use(express.static(process.cwd()+"/Public"));
 
--- Express_js/routes/api/members.js
+++ Express_js/routes/api/members.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { members } from "../../Members.js";
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
@@ -15,6 +16,23 @@ router.get("/:id", (req, res) => {
        res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
     }
     
+});
+
+// Create Member
+router.post("/", (req, res) => {
+      const newMember = {
+          id: uuidv4(),
+          name: req.body.name,
+          email: req.body.email,
+          status: "active"
+      };
+
+      if(!newMember.name || !newMember.email) {
+          return res.status(400).json({ msg: "Please include name and email." });
+      }
+
+      members.push(newMember);
+      res.json(members);
 });
 
 
