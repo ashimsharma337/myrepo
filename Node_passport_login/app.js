dcf70f502660f13fee54de3bdc1fb3c306ae1ea6
Ashim Sharma <ashimsharma337@gmail.com>
2021-12-18 11:45:21 +0545

    mongodb connected

--- Node_passport_login/app.js
+++ Node_passport_login/app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// DB config
+const db = require("./config/keys").mongoURI;
+
+// connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true })
+.then(() => console.log("MongoDB connected....."))
+.catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 
