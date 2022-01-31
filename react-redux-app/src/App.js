f549492da1f5dc42b73eb1ea47ce47992e2da081
Ashim Sharma <ashimsharma337@gmail.com>
2022-01-31 10:32:41 +0545

    project completed

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -1,9 +1,21 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProductListing from './containers/ProductListing';
+import Header from './containers/Header';
 import './App.css';
+import ProductDetail from './containers/ProductDetail';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/" exact element={<ProductListing/>}></Route>
+          <Route path="/product/:productId" exact element={<ProductDetail/>}></Route>
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
